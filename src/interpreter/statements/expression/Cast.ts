768834431e8f62235c94fed8c01f13772eac948e
Damian <damianpeaf@gmail.com>
2023-04-08 21:03:05 -0600

    feat: casts

--- src/interpreter/statements/expression/Cast.ts
+++ src/interpreter/statements/expression/Cast.ts
@@ -1,4 +1,4 @@
-import { PrimitiveT } from "../../elements";
+import { PrimitiveT, Symbols, TypeWiseValueType } from "../../elements";
 import { StatementArgs } from "../Statement";
 import { Value } from "../value";
 import { Expression } from "./Expression";
@@ -10,28 +10,128 @@ export type CastArgs = StatementArgs & {
 
 export class Cast extends Value {
 
-    public value: Expression;
-    public type: PrimitiveT;
+
+    private valueToConvert: Expression;
+    private conversionType: PrimitiveT;
+
+    private _v: any;
+    private _t: TypeWiseValueType;
 
     constructor({ value, type, ...args }: CastArgs) {
         super(args);
-        this.value = value;
-        this.type = type;
+        this.valueToConvert = value;
+        this.conversionType = type;
     }
 
-    get returnType(): any {
-        throw new Error("Method not implemented.");
-    }
     public getGrahpvizLabel(): string {
         return `Cast: ${this.type}`;
     }
     public getGrahpvizEdges(): string {
         return `
-            ${this.linkStatement(this.value)}
+            ${this.linkStatement(this.valueToConvert)}
         `
     }
-    public evaluate() {
-        throw new Error("Method not implemented.");
+
+    get type(): TypeWiseValueType {
+        return this._t;
+    }
+    get value(): any {
+        return this._v;
     }
 
+    // Possible conversions:
+    // Int a double
+    // Int a String
+    // Int a Char
+
+    // Double a Int
+    // Double a String
+
+    // Char a double
+    // Char a Int
+    public evaluate() {
+        this.valueToConvert.evaluate();
+        const originalType = this.valueToConvert.returnType;
+        const originalValue = this.valueToConvert.value;
+
+        // Same type cast
+        if (originalType === this.conversionType) {
+            this._v = originalValue;
+            this._t = originalType;
+            return;
+        }
+
+        // Null cast
+        if (originalType === Symbols.NULL) {
+            this._v = null;
+            this._t = Symbols.NULL;
+            return;
+        }
+
+        // Soported casts
+        if (originalType === Symbols.INT) {
+
+            if (this.conversionType === Symbols.DOUBLE) {
+                this._v = Number(originalValue);
+                this._t = Symbols.DOUBLE;
+                return;
+            }
+
+            if (this.conversionType === Symbols.STRING) {
+                this._v = originalValue.toString();
+                this._t = Symbols.STRING;
+                return;
+            }
+
+            if (this.conversionType === Symbols.CHAR) {
+                this._v = String.fromCharCode(originalValue);
+                this._t = Symbols.CHAR;
+                return;
+            }
+
+        }
+
+        if (originalType === Symbols.DOUBLE) {
+
+            if (this.conversionType === Symbols.INT) {
+                this._v = Math.floor(originalValue);
+                this._t = Symbols.INT;
+                return;
+            }
+
+            if (this.conversionType === Symbols.STRING) {
+                this._v = originalValue.toString();
+                this._t = Symbols.STRING;
+                return;
+            }
+
+        }
+
+
+        if (originalType === Symbols.CHAR) {
+
+            if (this.conversionType === Symbols.DOUBLE) {
+                this._v = Number(originalValue.charCodeAt(0));
+                this._t = Symbols.DOUBLE;
+                return;
+            }
+
+            if (this.conversionType === Symbols.INT) {
+                this._v = originalValue.charCodeAt(0);
+                this._t = Symbols.INT;
+                return;
+            }
+        }
+
+
+        this.context.errorTable.addError({
+            column: this.column,
+            line: this.line,
+            message: `No se puede realizar el casteo de ${originalType} a ${this.conversionType}`,
+            type: 'Semantico'
+        })
+
+        this._v = null;
+        this._t = Symbols.NULL;
+    }
 }
